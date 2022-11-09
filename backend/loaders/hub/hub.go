79d32a383d2ef2a64c065dab9242f008c0971378
Thun (MacBook Pro) <bsthun@gmail.com>
2022-11-09 18:21:29 +0700

    Add currentcard

--- backend/loaders/hub/hub.go
+++ backend/loaders/hub/hub.go
@@ -11,6 +11,7 @@ type Model struct {
 	Topics                   []*database.Topic `json:"topics"`
 	Teams                    []*database.Team  `json:"teams"`
 	Turned                   []*database.Team  `json:"-"`
+	CurentCard               *database.Card    `json:"-"`
 	AdminConn                *extend.ConnModel `json:"-"`
 	LeaderboardProjectorConn *extend.ConnModel `json:"-"`
 	CardProjectorConn        *extend.ConnModel `json:"-"`
