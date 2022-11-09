30bd2ebb94092cd384f0f0282db391a6b93df891
Jirasin Jarethammajit <69670650+jirasin02@users.noreply.github.com>
2022-11-09 14:51:14 +0700

    feat: add card page (#7)
    Co-authored-by: Floaty_KT <FloatKasemtan@users.noreply.github.com>
    Co-authored-by: Thun (MacBook Pro) <bsthun@gmail.com>
    Co-authored-by: Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>

---
+++ frontend/src/lib/components/MobileTopBar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { Team } from 'src/types/preview'
+
+	import backIcon from '../../assets/images/back-icon.png'
+
+	export let team: Team
+	export let selected: boolean
+	export let handleSelected: (selected: boolean) => void
+</script>
+
+<div
+	class="w-full h-11 pl-[30px] pr-[58px] flex justify-between items-center bg-[#1B2D51] font-semibold text-lg"
+>
+	<div class="flex justify-center items-center w-9">
+		{#if selected == true}
+			<button
+				on:click={() => {
+					handleSelected(selected)
+				}}
+			>
+				<img src={backIcon} alt="backIcon" />
+			</button>
+		{/if}
+	</div>
+	<div class="w-[300px] flex justify-center items-center">
+		<span class="text-white">{team.name}</span>
+	</div>
+</div>
