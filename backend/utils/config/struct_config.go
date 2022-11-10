15077c9fe844c392dc8f719dacbd798c20c54923
Thun (MacBook Pro) <bsthun@gmail.com>
2022-11-11 01:06:00 +0700

    Add student secret

--- backend/utils/config/struct_config.go
+++ backend/utils/config/struct_config.go
@@ -7,4 +7,5 @@ type config struct {
 	Cors            []string    `yaml:"cors" validate:"required"`
 	ProjectorSecret string      `yaml:"projector_secret" validate:"required"`
 	AdminSecret     string      `yaml:"admin_secret" validate:"required"`
+	StudentSecret   string      `yaml:"student_secret" validate:"required"`
 }
