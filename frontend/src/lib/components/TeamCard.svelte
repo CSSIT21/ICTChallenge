6d4e3b9757a0933f54624a3c3e1821269eedc398
Sirawit Pratoomsuwan <s36526@bj.ac.th>
2022-11-16 20:20:51 +0700

    feat: manager

---
+++ frontend/src/lib/components/TeamCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let id = 0
+	export let label = 'Team 1'
+	export let name = 'Benchamarachuthit'
+</script>
+
+<div
+	class="flex flex-row gap-2 h-[4rem] "
+	style="border-bottom: 1px solid rgba(217, 217, 217, .5)"
+>
+	<div class="w-[4rem] flex items-center justify-center">
+		<div
+			class="rounded-full w-[32px] h-[32px] flex justify-center items-center font-bold"
+			style="background-color: rgb(242, 242, 242); color: rgba(0,0,0,.4)"
+		>
+			{id}
+		</div>
+	</div>
+	<div class="flex flex-col items-center justify-center">
+		<b>{label}</b>
+		<span class="ml-2">{name}</span>
+	</div>
+</div>
--- frontend/src/pages/Admin.svelte
+++ frontend/src/pages/Admin.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import AdminContainer from 'src/lib/components/AdminContainer.svelte'
+	import AdminManager from 'src/lib/components/AdminManager.svelte'
+	let mode = false
 </script>
 
 <main
 	class="h-screen w-screen overflow-hidden bg-gradient-to-b from-[#3DC3B6] via-[#4F68BF] to-[#1B2D51]"
 >
-	<AdminContainer />
+	{#if mode}
+		<AdminContainer />
+	{:else}
+		<AdminManager />
+	{/if}
 </main>
